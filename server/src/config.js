996bd42a8c954356dc1f87773eb32bf5039be73c
Danya2121 <smandanial4@gmail.com>
2026-03-01 01:07:49 +0500

    Update config to remove CLIENT_ORIGIN
    Removed CLIENT_ORIGIN from config exports.

--- server/src/config.js
+++ server/src/config.js
@@ -1,6 +1,6 @@
 const path = require("path");
 
-const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || "http://localhost:5173";
-const UPLOADS_DIR = process.env.UPLOADS_DIR || path.join(__dirname, "..", "uploads");
+const UPLOADS_DIR =
+  process.env.UPLOADS_DIR || path.join(__dirname, "..", "uploads");
 
-module.exports = { CLIENT_ORIGIN, UPLOADS_DIR };
+module.exports = { UPLOADS_DIR };
