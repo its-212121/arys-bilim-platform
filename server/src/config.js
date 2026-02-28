683d7e477211f3ebcacc66736b531bd48467520b
Danya2121 <smandanial4@gmail.com>
2026-03-01 00:14:29 +0500

    Update config.js

--- server/src/config.js
+++ server/src/config.js
@@ -1,10 +1,6 @@
 const path = require("path");
 
-const CLIENT_ORIGINS = (process.env.CLIENT_ORIGINS || "http://localhost:5173")
-  .split(",")
-  .map((s) => s.trim())
-  .filter(Boolean);
-
+const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || "http://localhost:5173";
 const UPLOADS_DIR = process.env.UPLOADS_DIR || path.join(__dirname, "..", "uploads");
 
-module.exports = { CLIENT_ORIGINS, UPLOADS_DIR };
+module.exports = { CLIENT_ORIGIN, UPLOADS_DIR };
